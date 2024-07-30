bdeb07b9b4c57911fce4a57d4c4c239af061259c
Anas-Aqeel <anasaqeel.4444@gmail.com>
2024-07-30 22:10:06 +0500

    Auth Customization

---
+++ app/(auth)/layout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div className=" m-0  bg-[#52bced1f] lg:bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/212 p-6 sm:p-12">
+          <div className="flex gap-4 items-center justify-center md:justify-start mt-8 md:mt-0">
+            <img src="/images/loop.png" className="w-[80px] h-auto" />
+            <h4 className="text-3xl font-bold">Microsoft Loop</h4>
+          </div>
+
+          <div className="mt-12 flex flex-col items-center">{children}</div>
+        </div>
+        <div className="flex-1 bg-[#529bed1f] text-center hidden lg:flex">
+          <div
+            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+            style={{
+              backgroundImage: 'url("/images/login.svg")',
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
