c4d96e6e63f5f7c46ee885c81ad8a15129d2e9fa
Anas-Aqeel <anasaqeel.4444@gmail.com>
2024-08-20 14:54:35 +0500

    Favicon Updated

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,30 @@
-import { Outfit } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-const outfit = Outfit({ subsets: ['latin'] })
-import { Toaster } from "@/components/ui/toaster"
+import { Outfit } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+const outfit = Outfit({ subsets: ['latin'] });
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata = {
-  title: 'Screw Sync',
+  title: 'Microsoft Loop',
   description: 'Real-time Collaboration Platform',
-}
+  icons: {
+    icon: '/images/loop.png',
+  },
+  icon: '/images/loop.png',
+};
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/images/loop.png" />
+        </head>
         <body className={`${outfit.className}`}>
           {children}
           <Toaster />
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- components/ui/toaster.jsx
+++ components/ui/toaster.jsx
@@ -17,8 +17,8 @@ export function Toaster() {
     (<ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          (<Toast key={id} {...props}>
-            <div className="grid gap-1">
+          (<Toast key={id} {...props} className="z-[10000]">
+            <div className="grid gap-1 z-[10000]">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
